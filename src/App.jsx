c5bdf3eb273e863ba33f2412779aaae846d50511
Joshua <jagschell2000@gmail.com>
2023-07-27 21:20:22 +0200

    Componetized navbar and hero secion

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,13 @@
 import "./App.css";
+import Navigation from "./components/Navigation";
+import HeroSection from "./components/Pages/HeroSection/HeroSection";
 
 export default function App() {
   return (
     <>
+      <Navigation />
       <div className="bg-[url('../src/assets/whalebackground.jpg')] bg-cover h-screen">
-        <div className="w-full bg-slate-950 text-white flex flex-row items-center h-10">
-          <div className="basis-2/3 px-10">Logo</div>
-          <div className="basis-1/3 flex flex-row bg-slate-400 h-full items-center">
-            <div className="px-8">Home</div>
-            <div className="px-8">About</div>
-            <div className="px-8">Contact</div>
-          </div>
-        </div>
-
-        <div className="w-full h-full flex items-center justify-center">
-          <div className="w-[100vh] h-[85vh] p-6 text-white bg-white rounded-lg shadow bg-opacity-20 backdrop-blur-lg drop-shadow-lg">
-            <div className="grid grid-cols-2 gap-4">
-              <div className="w-[100%] h-[100%]">
-                <img src="../src/assets/ME.png" alt="me" className="h-fit" />
-              </div>
-              <div>
-                <h1 className="uppercase">welcome joshua schell here</h1>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="bg-gray-400 h-screen">
-        <div className="text-white">
-          <h1>Second Page</h1>
-        </div>
+        <HeroSection />
       </div>
     </>
   );
