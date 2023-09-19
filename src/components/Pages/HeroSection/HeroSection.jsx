533f60a76347ab9a07bbfe1be7adec51054bdf5b
Joshua <jagschell2000@gmail.com>
2023-09-19 22:04:05 +0200

    add grid to 2 elements

--- src/components/Pages/HeroSection/HeroSection.jsx
+++ src/components/Pages/HeroSection/HeroSection.jsx
@@ -3,20 +3,25 @@ export default function HeroSection() {
     <>
       <div className="w-full h-[100%] flex justify-center items-center pt-10">
         <div className="w-[75%] h-[75%] p-6 text-white bg-white rounded-lg shadow bg-opacity-20 backdrop-blur-lg drop-shadow-lg">
-          <div className="w-full h-full">
-            <div className="">
-              <img
-                src="../src/assets/ME.png"
-                alt="me"
-                className="h-72 rounded-xl shadow-xl"
-              />
+          <div className="grid grid-cols-2 gap-2 h-full">
+            <div className=" text-white w-30 h-full flex justify-center items-center">
+              Hello
             </div>
-            <div>
-              <h1 className="uppercase">welcome joshua schell here</h1>
+
+            <div className="w-30 text-[75px] p-5 drop-shadow-lg flex items-center">
+              Welcome, Take A Look Around!
             </div>
           </div>
         </div>
       </div>
     </>
   );
+}
+
+{
+  /* <img
+                src="../src/assets/ME.png"
+                alt="me"
+                className="h-[600px] rounded-xl shadow-xl"
+              /> */
 }
