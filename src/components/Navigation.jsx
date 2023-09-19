e78509dba2f63193661ef449561b63639c2c9681
Joshua <jagschell2000@gmail.com>
2023-09-19 22:03:15 +0200

    mapped items from App

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,14 +1,17 @@
-export default function Navigation() {
+const Navigation = ({ items }) => {
   return (
     <>
-      <div className="w-full bg-slate-950 text-white flex flex-row items-center h-12 fixed top-0 left-0 right-0">
-        <div className="basis-2/3 px-10">Logo</div>
-        <div className="basis-1/3 flex flex-row bg-slate-400 h-full items-center">
-          <div className="px-8">Home</div>
-          <div className="px-8">About</div>
-          <div className="px-8">Contact</div>
-        </div>
+      <div className="bg-white text-white shadow bg-opacity-20 backdrop-blur-lg drop-shadow-lg w-full flex justify-end flex-row p-5 fixed z-40">
+        <ul className="flex flex-row space-x-8 mr-[165px]">
+          {items.map((item, index) => (
+            <li key={index} className="drop-shadow-lg mx-2">
+              <a href={item.link}>{item.text}</a>
+            </li>
+          ))}
+        </ul>
       </div>
     </>
   );
-}
+};
+
+export default Navigation;
