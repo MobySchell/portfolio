baf7eda8bd99b9e89cf1b74e6eaad11c4a15eb4e
Joshua <jagschell2000@gmail.com>
2023-09-19 22:03:35 +0200

    initial

---
+++ src/components/Pages/AboutSection/AboutSection.jsx
@@ -0,0 +1,9 @@
+export default function AboutSection() {
+  return (
+    <>
+      <div className="w-full h-screen flex justify-center items-center">
+        AboutSection
+      </div>
+    </>
+  );
+}
