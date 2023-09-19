a46c26386349199e711c6160f0c582ffd8d37fca
Joshua <jagschell2000@gmail.com>
2023-09-19 22:02:25 +0200

    Added Navbar elements

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
 import "./App.css";
 import Navigation from "./components/Navigation";
 import HeroSection from "./components/Pages/HeroSection/HeroSection";
+import AboutSection from "./components/Pages/AboutSection/AboutSection";
 
 export default function App() {
+  const navbarItems = [
+    { text: "Home", link: "/" },
+    { text: "About", link: "/about" },
+    { text: "Services", link: "/services" },
+    { text: "Contact", link: "/contact" },
+  ];
+
   return (
     <>
-      <Navigation />
-      <div className="bg-[url('../src/assets/whalebackground.jpg')] bg-cover h-screen">
+      <Navigation items={navbarItems} />
+      <div className="bg-[url('../src/assets/whalebackground.jpg')] bg-fixed bg-repeat-x h-screen">
         <HeroSection />
+        <AboutSection />
       </div>
     </>
   );
