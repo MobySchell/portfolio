e2a0f32e929584f584eafe9dce8a02a98a60d262
Joshua <jagschell2000@gmail.com>
2023-09-20 23:24:25 +0200

    made bg img fixed

--- src/components/Pages/AboutSection/AboutSection.jsx
+++ src/components/Pages/AboutSection/AboutSection.jsx
@@ -1,9 +1,22 @@
 export default function AboutSection() {
   return (
     <>
-      <div className="w-full h-screen flex justify-center items-center">
-        AboutSection
+      <div className="w-full h-screen flex justify-center items-center bg-[#456E84]">
+        <div className="grid grid-cols-2 w-[80%] h-[75%]">
+          <div className="bg-white rounded-l-lg">Headshot</div>
+          <div className="bg-slate-600 rounded-r-lg">
+            <div>Key Skills</div>
+            <div>Expertise</div>
+            <div>Achievements</div>
+          </div>
+        </div>
       </div>
     </>
   );
 }
+
+// Todo: Headshot
+// Todo: Key skills and expertise
+// Achievements
+// Work Philosophy
+// More sections - Hobbies, Testimonials, contact information etc.
