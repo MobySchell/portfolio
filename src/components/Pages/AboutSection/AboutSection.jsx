d76fdc52d8aeb88c3c762c94b3687f9aacdcc744
Joshua <jagschell2000@gmail.com>
2023-09-22 08:04:53 +0200

    Added 2 cols and 3 rows to right section

--- src/components/Pages/AboutSection/AboutSection.jsx
+++ src/components/Pages/AboutSection/AboutSection.jsx
@@ -3,12 +3,22 @@ export default function AboutSection() {
     <>
       <div className="w-full h-screen flex justify-center items-center bg-[#456E84]">
         <div className="grid grid-cols-2 w-[80%] h-[75%]">
-          <div className="bg-white rounded-l-lg">Headshot</div>
-          <div className="bg-slate-600 rounded-r-lg">
-            <div>Summary Of Myself</div>
-            <div className="">
-              <div>Key Skills</div>
-              <div>Achievements</div>
+          <div className="bg-white rounded-l-lg flex justify-center items-center">
+            <div className="bg-slate-300 w-[80%] h-[80%] rounded-lg">
+              Headshot
+            </div>
+          </div>
+          <div className="bg-slate-600 rounded-r-lg flex justify-center items-center">
+            <div className="bg-gray-500 w-[80%] h-[80%] grid grid-cols-2 grid-rows-3">
+              <div className="col-span-2 bg-red-600">Summary Of Myself</div>
+              <div className="grid grid-cols-2 gap-2 col-span-2">
+                <div className="bg-amber-400">Key Skills</div>
+                <div className="bg-amber-400">Achievements</div>
+              </div>
+              <div>
+                <button>Test</button>
+                <button>Test</button>
+              </div>
             </div>
           </div>
         </div>
