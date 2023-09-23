3872c23efbb4ed3769fcfbef5edc546ba97459e4
Joshua <jagschell2000@gmail.com>
2023-09-23 09:13:55 +0200

    initial

--- src/components/Pages/ContactSection.jsx
+++ src/components/Pages/ContactSection.jsx
@@ -1,3 +1,9 @@
 export default function ContactSection() {
-  return <div>ContactSection</div>;
+  return (
+    <>
+      <div className="w-full h-screen flex justify-center items-center">
+        Contact
+      </div>
+    </>
+  );
 }
