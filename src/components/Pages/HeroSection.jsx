f4996b67243499ce0f911d8aaf887424552963e9
Joshua <jagschell2000@gmail.com>
2023-09-24 16:26:55 +0200

    configured new layout

--- src/components/Pages/HeroSection.jsx
+++ src/components/Pages/HeroSection.jsx
@@ -2,14 +2,14 @@ export default function HeroSection() {
   return (
     <>
       <div className="w-full h-[100%] flex justify-center items-center pt-10">
-        <div className="w-[75%] h-[75%] p-6 text-white bg-white rounded-lg shadow bg-opacity-20 backdrop-blur-lg drop-shadow-lg">
+        <div className="w-[80%] h-[75%] p-6 text-white bg-white rounded-lg shadow bg-opacity-20 backdrop-blur-lg drop-shadow-lg">
           <div className="grid grid-cols-2 gap-2 h-full">
-            <div className=" text-white w-30 h-full flex justify-center items-center">
-              Hello
+            <div className=" text-white w-30 h-full flex justify-center items-center bg-black">
+              picture
             </div>
 
-            <div className="w-30 text-[75px] p-5 drop-shadow-lg flex items-center">
-              Welcome, Take A Look Around!
+            <div className="w-30 drop-shadow-lg flex justify-center items-center">
+              Summary
             </div>
           </div>
         </div>
