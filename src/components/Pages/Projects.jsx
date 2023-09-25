b32c6d5771091671245bb02d249dfd7f618cd651
Joshua <jagschell2000@gmail.com>
2023-09-25 20:05:12 +0200

    added placeholder for project section

---
+++ src/components/Pages/Projects.jsx
@@ -0,0 +1,17 @@
+export default function Projects() {
+  return (
+    <>
+      <div className="w-full h-screen flex justify-center items-center bg-[#213d8a] flex-col">
+        <p className="text-white text-2xl p-6">
+          Currently Working On Fixing All Projects
+        </p>
+
+        <img
+          src="../src/assets/construction.gif"
+          alt="construction funny"
+          className="h-[50%]"
+        />
+      </div>
+    </>
+  );
+}
