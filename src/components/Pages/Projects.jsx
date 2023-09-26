b4ab5795f553a3a774cf635f3e8bc167b4471ebf
Joshua <jagschell2000@gmail.com>
2023-09-26 12:48:55 +0200

    added gradient background

--- src/components/Pages/Projects.jsx
+++ src/components/Pages/Projects.jsx
@@ -1,7 +1,7 @@
 export default function Projects() {
   return (
     <>
-      <div className="w-full h-screen flex justify-center items-center bg-[#213d8a] flex-col">
+      <div className="w-full h-screen flex justify-center items-center bg-gradient-to-r from-[#EFF3F5] from-20% via-[#07162B] via-70% to-[#264A61] to-80% flex-col">
         <p className="text-white text-2xl p-6">
           Currently Working On Fixing All Projects
         </p>
