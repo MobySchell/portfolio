2208c1ac4e8999f7942608c07e9c9b6149a84563
Joshua <jagschell2000@gmail.com>
2023-09-27 13:46:25 +0200

    added hover effect

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -2,10 +2,15 @@ const Navigation = ({ items }) => {
   return (
     <>
       <div className="bg-white text-white shadow bg-opacity-20 backdrop-blur-lg drop-shadow-lg w-full flex justify-end flex-row p-5 fixed z-40">
-        <ul className="flex flex-row space-x-8 mr-[165px]">
+        <ul className="flex flex-row mr-24">
           {items.map((item, index) => (
-            <li key={index} className="drop-shadow-lg mx-2">
-              <a href={item.link}>{item.text}</a>
+            <li key={index} className="drop-shadow-lg mx-5 p-1">
+              <a
+                className="hover:text-gray-950 bg-gradient-to-r hover:from-pink-500 hover:to-yellow-500 rounded-md py-1 px-4"
+                href={item.link}
+              >
+                {item.text}
+              </a>
             </li>
           ))}
         </ul>
