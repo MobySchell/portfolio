2509527c8c886a7f93f3c62de6a2efc267149b17
Joshua Schell <jagschell2000@gmail.com>
2024-01-31 21:32:32 +0200

    Positioning

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,7 +1,12 @@
 export default function Navigation() {
   return (
     <>
-      <div>Navigation</div>
+      <div className="absolute top-0 bg-secondary w-screen">
+        <div className="flex w-[100%] px-5 py-2">
+          <div className="justify-start">Logo</div>
+          <div className="justify-end">Items</div>
+        </div>
+      </div>
     </>
   );
 }
