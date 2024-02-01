006948df623fc2a2d6406534a86fff20946cc519
Joshua <jagschell2000@gmail.com>
2024-02-01 22:25:18 +0200

    started on layout for Home page

--- src/components/pages/LandingPage.tsx
+++ src/components/pages/LandingPage.tsx
@@ -1,7 +1,14 @@
 function LandingPage() {
   return (
     <>
-      <div></div>
+      <div className="grid w-[100%] h-screen bg-primary">
+        <div className="place-self-center w-[80%] h-[80%] bg-secondary">
+          <div>
+            <div></div>
+            <div></div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
