b843131334c52aff78d75f6ca485c3b36adfd76f
Joshua <jagschell2000@gmail.com>
2024-02-01 08:49:44 +0200

    ...

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,7 +1,22 @@
+import { useEffect } from "react";
+
+/* eslint-disable react/prop-types */
 const Navigation = ({ items }) => {
+  useEffect(() => {
+    const nav = document.getElementById("nav");
+
+    setTimeout(() => {
+      window.addEventListener("load", function () {
+        nav.style.transform = "translateX(0)";
+      });
+    }, 100);
+  });
   return (
     <>
-      <div className="bg-white text-white shadow bg-opacity-20 backdrop-blur-lg drop-shadow-lg w-full flex justify-end flex-row p-5 fixed z-40">
+      <div
+        id="nav"
+        className="bg-white text-white shadow bg-opacity-20 backdrop-blur-lg drop-shadow-lg w-full flex justify-end flex-row p-5 fixed z-40 translate-x-[100%] duration-1000"
+      >
         <ul className="flex flex-row mr-24">
           {items.map((item, index) => (
             <li key={index} className="drop-shadow-lg mx-5 p-1">
