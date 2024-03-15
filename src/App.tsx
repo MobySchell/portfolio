f9023451f61a5f59b19626514f2b94cc1da33f58
Josh <jagschell2000@gmail.com>
2024-03-15 14:09:21 +0200

    Added more routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import "./App.css";
 import Navigation from "./components/Navigation";
 import LandingPage from "./components/pages/LandingPage";
+import AboutMe from "./components/pages/AboutMe";
+import Contact from "./components/pages/Contact";
+import Projects from "./components/pages/Projects";
 import { Routes, Route } from "react-router-dom";
 
 function App() {
@@ -9,6 +12,9 @@ function App() {
       <Navigation />
       <Routes>
         <Route path="/" element={<LandingPage />} />
+        <Route path="/about" element={<AboutMe />} />
+        <Route path="/projects" element={<Contact />} />
+        <Route path="/contact" element={<Projects />} />
       </Routes>
     </>
   );
