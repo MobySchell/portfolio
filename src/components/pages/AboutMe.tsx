7b02e601ec951e4161f939020bba6258e9e7d8ab
Josh <jagschell2000@gmail.com>
2024-03-18 17:59:17 +0200

    added content

--- src/components/pages/AboutMe.tsx
+++ src/components/pages/AboutMe.tsx
@@ -1,3 +1,13 @@
 export default function AboutMe() {
-  return <div>AboutMe</div>;
+  return (
+    <>
+      <div className="grid w-[100%] h-screen bg-primary">
+        <div className="place-self-center w-[80%] h-[80%] bg-action">
+          <div className="grid place-content-center w-[100%] h-[100%]">
+            <div className="text-right text-white">About Me</div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
