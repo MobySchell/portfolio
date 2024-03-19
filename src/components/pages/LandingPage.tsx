f3939607bcef46913289ed4efc26a3df27d65b9d
Josh <jagschell2000@gmail.com>
2024-03-19 15:09:11 +0200

    added welcome message with animation

--- src/components/pages/LandingPage.tsx
+++ src/components/pages/LandingPage.tsx
@@ -3,10 +3,20 @@ function LandingPage() {
     <>
       <div className="grid w-[100%] h-screen bg-primary">
         <div className="place-self-center w-[80%] h-[80%] bg-action">
-          <div className="grid grid-cols-3 place-content-center w-[100%] h-[100%]">
-            <div className="text-right text-white">Welcome message</div>
-            <div className="text-center text-white">|</div>
-            <div className="test-left text-white">Details</div>
+          <div className="grid place-content-center w-[100%] h-[100%] gap-5">
+            <div className="font-extrabold text-3xl md:text-4xl [text-wrap:balance] bg-clip-text text-transparent bg-gradient-to-r from-slate-200/60 to-50% to-slate-200">
+              Hi I'm Joshua{" "}
+              <span className="text-white inline-flex flex-col h-[calc(theme(fontSize.3xl)*theme(lineHeight.tight))] md:h-[calc(theme(fontSize.4xl)*theme(lineHeight.tight))] overflow-hidden">
+                <ul className="block animate-text-slide-5 text-left leading-tight [&_li]:block">
+                  <li>a Web Developer</li>
+                  <li>a Leader</li>
+                  <li>a Mentor</li>
+                  <li>a Hardware Guru</li>
+                  <li>an Explorer</li>
+                  <li aria-hidden="true">a Web Developer</li>
+                </ul>
+              </span>
+            </div>
           </div>
         </div>
       </div>
