628eabaf67de3f23089bbe1147bfec7c5091b77e
Josh <jagschell2000@gmail.com>
2024-03-23 10:00:39 +0200

    made more responsive

--- src/components/pages/Projects.tsx
+++ src/components/pages/Projects.tsx
@@ -2,7 +2,7 @@ export default function Projects() {
   return (
     <>
       <div className="grid w-[100%] h-screen bg-primary">
-        <div className="place-self-center w-[80%] h-[80%] bg-action p-5">
+        <div className="w-[90%] md:w-[80%] h-[80%] place-self-center bg-action rounded-lg p-5 mt-[32px]">
           <div className="grid place-content-center w-[100%] h-[100%]">
             <div className=" text-white">
               Currently Still Building Projects To Showcase 👷
